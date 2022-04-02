5fa46abe5332b07f99fc524fb275642b71a17333
LakshOPHS <68946802+LakshPatelOPHS1@users.noreply.github.com>
2022-04-02 16:38:07 +0100

    Update main.py

--- Script/main.py
+++ Script/main.py
@@ -11,23 +11,23 @@ driverService = Service("chromedriver.exe") # Download this!!!!!!!!!!
 
 browser = webdriver.Chrome(service=driverService, options=option)
 
-browser.get("https://www.drfrostmaths.com/timestables-game.php")
+browser.get("https://www.drfrostmaths.com/timestables-game.php") # Opens to Dr Frost
 
-go = input("Log in twat. \nEnter y to continue ")
+go = input("Log in twat. \nEnter y to continue ") # Waits until logged in. If not logged in, HTML element will not be found. (Line 21)
 while go.lower() != "y":
     go = input("Log in twat. \nEnter y to continue ")
 
 MaxNum = int(input("Enter max right answers "))
-START = browser.find_element(by=By.CLASS_NAME, value="very-large-button-variant")
-START.click()
+START = browser.find_element(by=By.CLASS_NAME, value="very-large-button-variant") # Start Button
+START.click() 
 
 for i in range(MaxNum):
     sleep(0.01)
-    question = browser.find_element(by=By.ID, value="question")
-    ans = browser.find_element(by=By.ID, value="calculator-display")
-
+    question = browser.find_element(by=By.ID, value="question") # The question as a string
+    ans = browser.find_element(by=By.ID, value="calculator-display") # The entry field
+ 
     if "×" in question.text:
-        ans.send_keys(int(eval(question.text.replace("×", "*"))))
+        ans.send_keys(int(eval(question.text.replace("×", "*")))) # Finds answer
     else:
-        ans.send_keys(int(eval(question.text.replace("÷", "/"))))
+        ans.send_keys(int(eval(question.text.replace("÷", "/")))) # Finds answer
 
